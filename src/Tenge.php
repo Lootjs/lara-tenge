eabaa82a28d87b7d1c4ddd92555368e46769d68d
Loot <77028277827@yandex.ru>
2019-12-03 23:25:10 +0600

    added return type for main class

--- src/Tenge.php
+++ src/Tenge.php
@@ -14,7 +14,7 @@ class Tenge {
      *
      * @return DriverInterface
      */
-    public static function with(string $driver) {
+    public static function with(string $driver): DriverInterface {
         if (! array_key_exists($driver, config('tenge.drivers', []))) {
             throw new \Exception(sprintf('Driver [%s] not found', $driver));
         }
